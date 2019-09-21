80fde57eabe2915049f28e3cfcd4c13919a6942e
Giorgio Zanni <g.zanni@hotmail.com>
2019-09-21 18:19:21 -0300

    FIX: styling issues

--- src/components/Congratulations/Congratulations.js
+++ src/components/Congratulations/Congratulations.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 import Button from '../Button'
 
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 const Congratulations = ({ logout, name }) => (
-  <div>
+  <Container>
     <h1>Congratulations {name}!</h1>
     <p>You've signed in successfully.</p>
-    <Button onClick={logout}>Sign out</Button>
-  </div>
+    <Button onClick={logout} fullWidth={false}>
+      Sign out
+    </Button>
+  </Container>
 )
 
 Congratulations.propTypes = {
