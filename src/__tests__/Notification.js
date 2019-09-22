2f44f2de040765a333abf987aea232ef82dc8956
Giorgio Zanni <g.zanni@hotmail.com>
2019-09-22 13:45:16 -0300

    FIX: settimeout in tests

--- src/__tests__/Notification.js
+++ src/__tests__/Notification.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, render, wait } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import Notification from '../components/Notification'
@@ -12,26 +12,28 @@ test('It remains hidden if there is no message', () => {
   expect(getByTestId('notification')).not.toBeVisible()
 })
 
-test("It's visible when there's a message", () => {
+test("It's visible when there's a message", async () => {
   const timeout = 5
   const message = 'Error'
   const { getByText } = render(
     <Notification message={message} timeout={timeout} />
   )
 
-  setTimeout(() => {
-    expect(getByText(message)).toBeVisible()
-  }, 200)
+  await wait(() => {
+    expect(getByText(message)).toHaveStyle(
+      `animation: notification ${timeout}s linear`
+    )
+  })
 })
 
-test('It hides after timeout', () => {
+test('It hides after timeout', async () => {
   const message = 'test'
   const timeout = 1
   const { getByText } = render(
     <Notification message={message} timeout={timeout} />
   )
 
-  setTimeout(() => {
+  await wait(() => {
     expect(getByText(message)).not.toBeVisible()
-  }, timeout * 1000)
+  })
 })
